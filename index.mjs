0efc3884f1ae2f53280aba98a82d523157a30512
Zara-Y <>
2026-03-11 07:27:55 -0700

    fix vercel backend deployment

--- index.mjs
+++ index.mjs
@@ -1,13 +1,18 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/index.mjs";
+import db from "./config/db.mjs"
 
 const app = express();
 
 
-    app.use(cors());
-    app.use(express.json());
-    app.use('/', router);
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
 
+// db connection
+db.connection
+    .once("open", () => console.log("Connected to DB"))
+    .on("Error", (err) => console.log("Error connecting to DB ==>", err));
 
 export default app;
--- server.mjs
+++ server.mjs
@@ -1,13 +1,13 @@
 import app from "./index.mjs"
-import db from "./config/db.mjs"
+// import db from "./config/db.mjs"
 
 
 
 const PORT=process.env.PORT || 3000;
 
-db.connection
-.once("open", () => console.log("Connected to DB"))
-.on("Error", (err) => console.log("Error connecting to DB ==>" , err));
+// db.connection
+// .once("open", () => console.log("Connected to DB"))
+// .on("Error", (err) => console.log("Error connecting to DB ==>" , err));
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
